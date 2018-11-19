20346ae2e20261a4acaf306f8678e81a6bf273d6
dingsycd <ding@yeah.net>
2018-11-19 17:36:02 +0800

    first commit

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+/**
+ * <p>Description:  </p>
+ * <p>Date: 2018年11月19日</p>
+ * @author dingd
+ *
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+		String string = "aa";
+		System.out.println(111);
+	}
+
+}
