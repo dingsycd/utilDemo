4d6bb36cb0bfd29c49515401f7375713b1da1eed
dingsycd <ding@yeah.net>
2018-11-30 16:23:16 +0800

    注释掉以前的代码

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,4 +1,7 @@
 package demo;
+
+import java.util.Objects;
+
 /**
  * <p>Description:  </p>
  * <p>Date: 2018年11月19日</p>
@@ -8,19 +11,19 @@ package demo;
 public class Demo {
 
 	public static void main(String[] args) {
-		String input = "[[\"[10, 15, 8]\",\"[11, 16, 9]\"]]";
-		System.out.println(input);
-		String sub = input.substring(4,input.length() - 4);
-		System.out.println(sub);
-		/**
-		 * | .  *  [] \  是特殊字符，在使用时要进行转义
-		 *   解决办法   \\[
-		 * 如：String [] = timeArr.split("\\[");
-		 */
-		String[] strings = sub.split("\\]\",\"\\[");
-		for (int i = 0; i < strings.length; i++) {
-			System.out.println(strings[i]);
-		}
+//		String input = "[[\"[10, 15, 8]\",\"[11, 16, 9]\"]]";
+//		System.out.println(input);
+//		String sub = input.substring(4,input.length() - 4);
+//		System.out.println(sub);
+//		/**
+//		 * | .  *  [] \  是特殊字符，在使用时要进行转义
+//		 *   解决办法   \\[
+//		 * 如：String [] = timeArr.split("\\[");
+//		 */
+//		String[] strings = sub.split("\\]\",\"\\[");
+//		for (int i = 0; i < strings.length; i++) {
+//			System.out.println(strings[i]);
+//		}
 //		System.out.println(1);
 //		System.out.println("a");
 //		System.out.println(11);
