08f7eb0daaea746ea155131d8a17bd665df51a47
dingding <ding914@yeah.net>
2019-03-07 22:12:31 +0800

    增加String的API测试

---
+++ src/main/java/demo/StringTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ *  字符串API测试
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        //测试compareTo方法
+        String s1 = "haa";
+        String s2 = "ha1";
+        System.out.println(s1.compareTo(s2));
+    }
+}
